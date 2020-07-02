b5785dca2e7bec182d0d1a599c2faae799fc4860
Jareena shaik <jareena78694@gmail.com>
2020-07-02 11:48:28 +0530

    "Second commit"

--- project/dbconnect.py
+++ project/dbconnect.py
@@ -3,7 +3,9 @@ import psycopg2
 def dbConnect():
     con = None
     try:
+        #reading config file
         params = config()
+        # connect method is used to connect to the database
         con = psycopg2.connect(**params)
     except(Exception, psycopg2.DatabaseError) as error:
         print(error)
